f303ed00b16e83332fac5a0b4bedf0bbd08be0af
radekaadek <radekaadek@gmail.com>
2023-07-18 03:13:00 +0200

    title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,11 @@
 	import LeafletMap from '$lib/LeafletMap.svelte';
 </script>
 
+<svelte:head>
+	<!-- title -->
+	<title>Hexapple!</title>
+</svelte:head>
+
 <main>
 	<!-- <img src="out1644.png" alt="out1644.png" id="a1644"> -->
 	<!-- each number from 1 to 6572 -->
